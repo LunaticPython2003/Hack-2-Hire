b59bec354c8be295808098effe4b2c0e3cd4def0
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-09-28 16:30:08 +0530

    v0.1.2 Created routing

--- Eshara/main.py
+++ Eshara/main.py
@@ -1,9 +1,13 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/convert')
 def test():
-    return "Hello World"
+    return render_template('converter.html')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 if __name__=="__main__":
     app.run()
