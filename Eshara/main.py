a946eab161e6c7add2c214fc5e3e55a791c70252
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-09-28 16:50:35 +0530

    v0.1.4 Rendered the video

--- Eshara/main.py
+++ Eshara/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, Response, render_template
+import cv2
+from scripts.camera import gen_frames
 
 app = Flask(__name__)
 @app.route('/converter')
@@ -8,6 +10,11 @@ def test():
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__=="__main__":
     app.run()
---
+++ Eshara/scripts/camera.py
@@ -0,0 +1,12 @@
+import cv2
+def gen_frames():  
+    video = cv2.VideoCapture(0)
+    while True:
+        success, frame = video.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
